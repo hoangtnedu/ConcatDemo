75b8b578af0dde90c8f8aab8631aa6f5d27c1def
HoangTN <HoangTN@DESKTOP-H68FGFI>
2021-08-10 16:07:24 +0700

    Initial commit

---
+++ src/concatdemo/ConcatDemo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package concatdemo;
+
+/**
+ *
+ * @author HoangTN
+ */
+public class ConcatDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String firstString = "This is";
+        String secondString = " a concatenated string.";
+        String thirdString = firstString+secondString;
+        System.out.println(thirdString);
+    }
+    
+}
